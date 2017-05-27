8c681f18ca350ae6e320d035ddf63ec734d078e8
chaospredictor <kuzi81@gmail.com>
2017-05-27 16:59:55 +0300

    main with arguments read input from stdin or a file

--- mtm_escape.c
+++ mtm_escape.c
@@ -7,16 +7,30 @@
 
 #include "mtm_escape.h"
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_NAME_LENG 200
 
-int main() {
+int main(int argc, char *argv[]) {
 
-	//FILE *file = fopen("temp.txt", "r");
-	char a[MAX_NAME_LENG];
-	//char b[MAX_NAME_LENG];
-	//char c[MAX_NAME_LENG];
 	FILE *file = stdin;
+
+	if ( !(argc == 1 || argc == 3 || argc == 5) ) {
+		printf("‫‪MTM_INVALID_COMMAND_LINE_PARAMETERS.‬‬\n");
+		//TODO change it to mtm_ex3 print function
+		return -1;
+	} else if ( argc == 5 ) {
+		if ( strcmp(argv[1],"-i") == 0 ) {
+			file = fopen(argv[2], "r");
+		} else if ( strcmp(argv[3],"-i") == 0 ) {
+			file = fopen(argv[4], "r");
+		}
+
+	}
+
+
+
+	char a[MAX_NAME_LENG];
 
 
 	//while ( fscanf(file,"%50s[^\n]%c", a, b)  != EOF ) {
