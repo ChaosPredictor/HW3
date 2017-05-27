123202b84b3a8d9526fffc3843dc6bf28bb7e2e0
chaospredictor <kuzi81@gmail.com>
2017-05-27 16:39:25 +0300

    main read line by line from file or command line

--- mtm_escape.c
+++ mtm_escape.c
@@ -8,6 +8,23 @@
 #include "mtm_escape.h"
 #include <stdio.h>
 
+#define MAX_NAME_LENG 200
+
 int main() {
-	printf("Hello World\n");
+
+	//FILE *file = fopen("temp.txt", "r");
+	char a[MAX_NAME_LENG];
+	//char b[MAX_NAME_LENG];
+	//char c[MAX_NAME_LENG];
+	FILE *file = stdin;
+
+
+	//while ( fscanf(file,"%50s[^\n]%c", a, b)  != EOF ) {
+	//while ( fgets(a, MAX_NAME_LENG, stdin) != NULL) {
+
+	while ( fgets(a, MAX_NAME_LENG, file) != NULL) {
+		printf("your input a: %s", a);
+		//printf("your input b: %s\n", b);
+		//printf("your input c: %s\n", c);
+	}
 }
