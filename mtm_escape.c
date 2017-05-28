c49b61d0752ab077878d7674850c51ce83ab1cd6
chaospredictor <kuzi81@gmail.com>
2017-05-28 17:57:49 +0300

    remove leading spaces

--- mtm_escape.c
+++ mtm_escape.c
@@ -8,6 +8,7 @@
 #include "mtm_escape.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_NAME_LENG 200
 
@@ -18,6 +19,7 @@ int main(int argc, char *argv[]) {
 	if ( !(argc == 1 || argc == 3 || argc == 5) ) {
 		printf("‫‪MTM_INVALID_COMMAND_LINE_PARAMETERS.‬‬\n");
 		//TODO change it to mtm_ex3 print function
+		fclose(file);
 		return -1;
 	} else if ( argc == 5 ) {
 		if ( strcmp(argv[1],"-i") == 0 ) {
@@ -25,20 +27,31 @@ int main(int argc, char *argv[]) {
 		} else if ( strcmp(argv[3],"-i") == 0 ) {
 			file = fopen(argv[4], "r");
 		}
+		if ( file == NULL ) {
+			printf("MTM_CANNOT_OPEN_FILE.");
+			//TODO change it to mtm_ex3 print function
+			fclose(file);
+			return -1;
+		}
 
+		//TODO output
 	}
-
-
 
 	char a[MAX_NAME_LENG];
 
-
-	//while ( fscanf(file,"%50s[^\n]%c", a, b)  != EOF ) {
-	//while ( fgets(a, MAX_NAME_LENG, stdin) != NULL) {
-
 	while ( fgets(a, MAX_NAME_LENG, file) != NULL) {
-		printf("your input a: %s", a);
-		//printf("your input b: %s\n", b);
-		//printf("your input c: %s\n", c);
+
+		const char* firstNonSpace = a;
+		while(*firstNonSpace != '\0' && isspace(*firstNonSpace)) {
+			++firstNonSpace;
+		}
+		size_t len = strlen(firstNonSpace)+1;
+		memmove(a, firstNonSpace, len);
+		if ( strcmp(a,"") != 0) {
+			printf("your input a: %s", a);
+		}
+
 	}
+
+	fclose(file);
 }
