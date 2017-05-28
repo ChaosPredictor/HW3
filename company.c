a73f53d30632fb967d4b4a982a495f71500985d3
chaospredictor <kuzi81@gmail.com>
2017-05-28 21:09:39 +0300

    company c&h added

---
+++ company.c
@@ -0,0 +1,21 @@
+/*
+ * company.c
+ *
+ *  Created on: May 28, 2017
+ *      Author: master
+ */
+
+#include "company.h"
+
+struct list_t {
+	char* email;
+	char* faculty;
+};
+
+//TODO all mails together
+
+
+
+MtmErrorCode add_company(char* email, char* faculty) {
+	return MTM_SUCCESS;
+}
---
+++ company.h
@@ -0,0 +1,25 @@
+/*
+ * company.h
+ *
+ *  Created on: May 28, 2017
+ *      Author: master
+ */
+
+#ifndef COMPANY_H_
+#define COMPANY_H_
+
+#include "mtm_ex3.h"
+
+
+//#include "visitor_room.h"
+//TODO add related files
+
+typedef struct company_t *Company;
+
+
+
+MtmErrorCode add_company(char* email, char* faculty);
+
+
+
+#endif /* COMPANY_H_ */
