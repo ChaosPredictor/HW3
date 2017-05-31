88f81ff973c1c51d33e512f059658b626740f498
chaospredictor <kuzi81@gmail.com>
2017-05-31 16:21:29 +0300

    escaper add

--- tests/user_test.c
+++ tests/user_test.c
@@ -1,41 +1,4 @@
-/*
-#include "../user.c"
-#include "./test_utilities.h"
-//#include "../set.h"
-#include "./../user.h"
-#include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include "./../mtm_escape.h"
-#include <string.h>
-#include <ctype.h>
-*/
 #include "./user_test.h"
-
-/*
-static ListElement copyString(ListElement str){
-	assert(str);
-	char* copy = malloc(strlen(str)+1);
-	return copy != NULL ? strcpy(copy, str) : NULL;
-}
-
-static void freeString(ListElement str){
-	free(str);
-}
-
-static bool isLongerThan(ListElement element, ListFilterKey number) {
-	char* string = element;
-    return strlen(string) > *(int*)number;
-}
-
-static int compareLexiStringLexicographical(ListElement element1, ListElement element2) {
-    return strcmp((char*)element1, (char*)element2);
-}
-
-static int compareStringLength(ListElement element1, ListElement element2) {
-    return strlen((char*)element1) - strlen((char*)element2);
-}*/
 
 
 static bool testCopyUser() {
@@ -135,12 +98,14 @@ static bool testAddUser() {
 	ASSERT_TEST( setGetSize(users) == 0 );
 	ASSERT_TEST( addUser(users, "company1@civil", CIVIL_ENGINEERING, -1 ) == MTM_INVALID_PARAMETER );
 	ASSERT_TEST( setGetSize(users) == 0 );
+	//Invalid email
 	ASSERT_TEST( addUser(users, "company1#civil", CIVIL_ENGINEERING, COMPANY ) == MTM_INVALID_PARAMETER);
 	ASSERT_TEST( setGetSize(users) == 0 );
 
+	//success company
 	ASSERT_TEST( addUser(users, "company1@civil", CIVIL_ENGINEERING, COMPANY ) == MTM_SUCCESS );
 	ASSERT_TEST( setGetSize(users) == 1 );
-	//same email
+	//fail - same email
 	ASSERT_TEST( addUser(users, "company1@civil", CIVIL_ENGINEERING, COMPANY ) == MTM_EMAIL_ALREADY_EXISTS );
 	ASSERT_TEST( setGetSize(users) == 1 );
 	ASSERT_TEST( addUser(users, "company1@civil", MECHANICAL_ENGINEERING, COMPANY ) == MTM_EMAIL_ALREADY_EXISTS );
@@ -149,13 +114,12 @@ static bool testAddUser() {
 	ASSERT_TEST( setGetSize(users) == 1 );
 	ASSERT_TEST( addUser(users, "company1@civil", MECHANICAL_ENGINEERING,  ESCAPER_2 ) == MTM_EMAIL_ALREADY_EXISTS );
 	ASSERT_TEST( setGetSize(users) == 1 );
-
+	//success other company same faculty
 	ASSERT_TEST( addUser(users, "company2@civil", CIVIL_ENGINEERING, COMPANY ) == MTM_SUCCESS );
 	ASSERT_TEST( setGetSize(users) == 2 );
-
+	//success escaper
 	ASSERT_TEST( addUser(users, "escaper1@civil", CIVIL_ENGINEERING, ESCAPER_1 ) == MTM_SUCCESS );
 	ASSERT_TEST( setGetSize(users) == 3 );
-
 	//same email
 	ASSERT_TEST( addUser(users, "escaper1@civil", MECHANICAL_ENGINEERING, ESCAPER_1 ) == MTM_EMAIL_ALREADY_EXISTS );
 	ASSERT_TEST( setGetSize(users) == 3 );
@@ -163,9 +127,11 @@ static bool testAddUser() {
 	ASSERT_TEST( setGetSize(users) == 3 );
 	ASSERT_TEST( addUser(users, "escaper1@civil", MECHANICAL_ENGINEERING, ESCAPER_2 ) == MTM_EMAIL_ALREADY_EXISTS );
 	ASSERT_TEST( setGetSize(users) == 3 );
-
+	//success escaper
 	ASSERT_TEST( addUser(users, "escaper2@civil", CIVIL_ENGINEERING, ESCAPER_1 ) == MTM_SUCCESS );
 	ASSERT_TEST( setGetSize(users) == 4 );
+	ASSERT_TEST( addUser(users, "escaper4@electrical", ELECTRICAL_ENGINEERING, ESCAPER_10 ) == MTM_SUCCESS );
+	ASSERT_TEST( setGetSize(users) == 5 );
 
 	setClear(users);
 	ASSERT_TEST( setGetSize(users) == 0 );
@@ -189,13 +155,54 @@ Set testHelperAddUsers() {
 	addUser( users, "company1@mathematics", MATHEMATICS, COMPANY );
 	addUser( users, "escaper1@civil", CIVIL_ENGINEERING, ESCAPER_1 );
 	addUser( users, "escaper2@civil", CIVIL_ENGINEERING, ESCAPER_5 );
-	addUser( users, "escaper3@civil", MECHANICAL_ENGINEERING, ESCAPER_2 );
-	ASSERT_TEST( setGetSize(users) == 12 );
+	addUser( users, "escaper3@mechanical", MECHANICAL_ENGINEERING, ESCAPER_2 );
+	addUser( users, "escaper1@electrical", ELECTRICAL_ENGINEERING, ESCAPER_10 );
+	addUser( users, "escaper1@biotechnology", BIOTECHNOLOGY_AND_FOOD_ENGINEERING, ESCAPER_1 );
+	addUser( users, "escaper1@aerospace", AEROSPACE_ENGINEERING, ESCAPER_7 );
+
+	ASSERT_TEST( setGetSize(users) == 15 );
 	return users;
 }
 
 static bool testRemoveCompany() {
 
+	Set users = testHelperAddUsers();
+
+	int numberOfUsers = setGetSize(users);
+	//invalid email
+	ASSERT_TEST( removeCompany( users, NULL ) ==  MTM_INVALID_PARAMETER );
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+	ASSERT_TEST( removeCompany( users, "company2#civil" ) ==  MTM_INVALID_PARAMETER );
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+	//company email does not exist
+	ASSERT_TEST( removeCompany( users, "company3@civil" ) ==  MTM_COMPANY_EMAIL_DOES_NOT_EXIST );
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+	//escaper email
+	ASSERT_TEST( removeCompany( users, "escaper1@civil" ) ==  MTM_COMPANY_EMAIL_DOES_NOT_EXIST );
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+	//TODO company with order
+
+	ASSERT_TEST( removeCompany( users, "company2@civil" ) ==  MTM_SUCCESS );
+	numberOfUsers--;
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+
+	ASSERT_TEST( removeCompany( users, "escaper1@civil" ) ==  MTM_COMPANY_EMAIL_DOES_NOT_EXIST );
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+
+	ASSERT_TEST( removeCompany( users, "company2@civil" ) ==  MTM_COMPANY_EMAIL_DOES_NOT_EXIST );
+	ASSERT_TEST( setGetSize(users) == numberOfUsers );
+
+	setClear(users);
+
+	ASSERT_TEST( setGetSize(users) == 0 );
+	setDestroy(users);
+	return true;
+}
+
+
+
+static bool testRemoveEscaper() {
+	//TODO
 	Set users = testHelperAddUsers();
 
 	int numberOfUsers = setGetSize(users);
@@ -226,7 +233,8 @@ static bool testRemoveCompany() {
 	return true;
 }
 
-static bool testFindFacultyFromEmail() {
+
+static bool testFindCompanyFacultyFromEmail() {
 
 	Set users = setCreate(copyUser, freeUser, compareUsers);
 	ASSERT_TEST( users != NULL);
@@ -333,8 +341,9 @@ int userTests (int argv, char** arc){
 	RUN_TEST(testCompareUser);
 	RUN_TEST(testAddUser);
 	RUN_TEST(testRemoveCompany);
+	RUN_TEST(testRemoveEscaper);
 	RUN_TEST(testEmailValidity);
-	RUN_TEST(testFindFacultyFromEmail);
+	RUN_TEST(testFindCompanyFacultyFromEmail);
 	RUN_TEST(testOtherSetUserFunctions);
 	return 0;
 }
